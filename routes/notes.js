b5d168f894318416416500c728b31d018eced4ef
ruriport <kanitabetaiw@gmail.com>
2024-02-05 11:51:09 +0900

    add cat api

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "***";
+const uri = "mongodb+srv://2101100120ob:j9nZHy_m@cluster0.qge2vmg.mongodb.net/?retryWrites=true&w=majorityty";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
